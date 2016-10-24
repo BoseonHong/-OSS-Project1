2b26b857ec11c129819e2926f1a7f0d3e87754c7
HyeonTae <dchs401@naver.com>
2016-10-24 17:03:44 +0900

    Update main.do more

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(){
+  
+  int a, b;
+  
   printf("Open Source SW Project\n");
   printf("First GitHub\n");
   printf("Hello Bo Suin!!!\n");
+  
+  printf("Give me the number a to b : ");
+  scanf("%d %d",&a,&b);
+  
+  printf("Good!! %d + %d = %d !!!\n",a,b,a+b);
+  
   return 0;
 }
