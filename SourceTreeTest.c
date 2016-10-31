ec13ebd35544649cc81f10e8a0d3b4f86fa63c01
DESKTOP-4GHGC44\swpc <goodcools34@gmail.com>
2016-10-31 17:10:39 +0900

    scanf() 함수 추가

--- SourceTreeTest.c
+++ SourceTreeTest.c
@@ -1,7 +1,17 @@
 ##include <stdio.h>
 
 int main(){
+  int num;
   printf("first oss code\n");
   printf("sceond code\n");
+
+  printf("select number(1~10)\n");
+  scanf("%d\n",&num );
+  if(num>0&&num<=10){
+    printf(" number = %d".num);
+  }
+  else{
+    printf("fail\n");
+  }
   return 0;
 }
